21fd05ed76a4eb14d4e89bb570bdb4a4abc81ded
traj3ctory <davidbenard.bd@gmail.com>
2022-02-06 11:20:56 +0100

    feat(component): :sparkles: create the user details component

---
+++ src/components/views/UserDetail.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="user_details">
+    <div>
+      <h5>Welcome back, {{ name }}</h5>
+      <small>Track, manage and forecast your customers and orders.</small>
+    </div>
+    <div>
+      <Button
+        label="Import"
+        icon="pi pi-upload"
+        class="p-button-sm p-button-raised p-button-text"
+      />
+      <Button
+        label="Add"
+        icon="pi pi-plus"
+        class="p-button-sm p-button-raised p-button-help text-white"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+import Button from "primevue/button";
+
+export default {
+  name: "UserDetail",
+  components: {
+    Button,
+  },
+  props: {
+    username: {
+      type: String,
+      required: false,
+      default: "Olivia",
+    },
+  },
+  data() {
+    return {
+      name: this.username,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+section.user_details {
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  button {
+    color: #222222;
+    font-size: 0.8rem;
+    margin-right: 0.4rem;
+  }
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,19 +1,26 @@
 <template>
-  <div class="wrapper">
-    <Header />
-    <Nav />
+  <div class="wrapper container-lg container-fluid">
+    <header>
+      <Header />
+      <Nav />
+    </header>
+    <main class="px-lg-4 px-md-3 px-1">
+      <UserDetail />
+    </main>
   </div>
 </template>
 
 <script>
-import Header from '../components/Layout/Header.vue'
-import Nav from '../components/Layout/Nav.vue'
+import Header from "../components/Layout/Header.vue";
+import Nav from "../components/Layout/Nav.vue";
+import UserDetail from "../components/views/UserDetail.vue";
 
 export default {
   name: "Dashboard",
   components: {
     Header,
     Nav,
+    UserDetail,
   },
 };
 </script>
